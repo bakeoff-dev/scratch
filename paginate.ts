5b6743f2708333c2fc7f8b0f151ffef9fc7cec9c
Shifan Hirani <shifan.hirani@gmail.com>
2026-09-22 22:15:04 -0500

    Add tail option to paginate

--- paginate.ts
+++ paginate.ts
@@ -1,3 +1,12 @@
-export function paginate<T>(items: T[], page: number, size: number): T[] {
+export function paginate<T>(
+  items: T[],
+  page: number,
+  size: number,
+  options: { tail?: boolean } = {},
+): T[] {
+  if (options.tail) {
+    return items.slice(-size);
+  }
+
   return items.slice((page - 1) * size, page * size);
 }
