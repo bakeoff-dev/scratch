8bfcd893d0b9c6e4aba6e3c01abd67169b0fc5d0
Shifan Hirani <shifan.hirani@gmail.com>
2026-09-22 22:50:56 -0500

    Add pageSlice pagination bounds export

--- paginate.test.ts
+++ paginate.test.ts
@@ -1,4 +1,5 @@
 import { expect, test } from 'bun:test';
-import { paginate } from './paginate';
+import { pageSlice, paginate } from './paginate';
 test('page 1 of size 2', () => { expect(paginate([1,2,3,4], 1, 2)).toEqual([1,2]); });
 test('page 2 of size 2', () => { expect(paginate([1,2,3,4], 2, 2)).toEqual([3,4]); });
+test('pageSlice returns the slice bounds for a page', () => { expect(pageSlice(1, 10)).toEqual({ start: 0, end: 10 }); });
--- paginate.ts
+++ paginate.ts
@@ -1,3 +1,8 @@
 export function paginate<T>(items: T[], page: number, size: number): T[] {
-  return items.slice((page - 1) * size, page * size);
+  const { start, end } = pageSlice(page, size);
+  return items.slice(start, end);
+}
+
+export function pageSlice(page: number, size: number): { start: number; end: number } {
+  return { start: (page - 1) * size, end: page * size };
 }
