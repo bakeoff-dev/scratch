f67c28b635b0908904e46bb4d04a3480c9d4c433
Shifan Hirani <shifan.hirani@gmail.com>
2026-09-23 23:18:14 -0500

    Add chunk helper to pagination module

--- paginate.test.ts
+++ paginate.test.ts
@@ -1,4 +1,10 @@
 import { expect, test } from 'bun:test';
-import { paginate } from './paginate';
+import { chunk, paginate } from './paginate';
 test('page 1 of size 2', () => { expect(paginate([1,2,3,4], 1, 2)).toEqual([1,2]); });
 test('page 2 of size 2', () => { expect(paginate([1,2,3,4], 2, 2)).toEqual([3,4]); });
+test('chunk splits items into arrays of at most the requested size', () => {
+  expect(chunk([1, 2, 3, 4, 5], 2)).toEqual([[1, 2], [3, 4], [5]]);
+});
+test('chunk returns no chunks for an empty array', () => {
+  expect(chunk([], 3)).toEqual([]);
+});
